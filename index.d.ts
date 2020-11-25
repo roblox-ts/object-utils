1fe11f40eb28bc8e75ef6a06d2b9deeff06ead2b
osyrisrblx <osyrisrblx@gmail.com>
2020-11-24 20:05:39 -0800

    Make functions use multiple signatures

--- index.d.ts
+++ index.d.ts
@@ -6,14 +6,7 @@ interface ObjectConstructor {
 	assign<A, B>(this: void, target: A, source: B): A & B;
 	assign<A, B, C>(this: void, target: A, source1: B, source2: C): A & B & C;
 	assign<A, B, C, D>(this: void, target: A, source1: B, source2: C, source3: D): A & B & C & D;
-	assign<A, B, C, D, E>(
-		this: void,
-		target: A,
-		source1: B,
-		source2: C,
-		source3: D,
-		source4: E,
-	): A & B & C & D & E;
+	assign<A, B, C, D, E>(this: void, target: A, source1: B, source2: C, source3: D, source4: E): A & B & C & D & E;
 	assign<A, B, C, D, E, F>(
 		this: void,
 		target: A,
@@ -29,58 +22,31 @@ interface ObjectConstructor {
 	 * Returns the names of the enumerable properties and methods of an object.
 	 * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
 	 */
-	keys<T extends object>(
-		this: void,
-		o: T,
-	): T extends Array<any>
-		? Array<number>
-		: T extends ReadonlySet<infer U>
-		? Array<U>
-		: T extends ReadonlyMap<infer K, any>
-		? Array<K>
-		: T extends ArrayLike<any>
-		? Array<number>
-		: keyof T extends never
-		? Array<unknown>
-		: Array<keyof T>;
+	keys<T>(this: void, o: ReadonlyArray<T>): Array<number>;
+	keys<T>(this: void, o: ReadonlySet<T>): Array<T>;
+	keys<K, V>(this: void, o: ReadonlyMap<K, V>): Array<K>;
+	keys<T extends object>(this: void, o: T): keyof T extends never ? Array<unknown> : Array<keyof T>;
 
 	/**
 	 * Returns an array of values of the enumerable properties of an object
 	 * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
 	 */
-	values<T extends object>(
-		this: void,
-		o: T,
-	): T extends Array<infer U>
-		? Array<NonNullable<U>>
-		: T extends ReadonlySet<any>
-		? Array<true>
-		: T extends ReadonlyMap<any, infer V>
-		? Array<NonNullable<V>>
-		: T extends ArrayLike<infer W>
-		? Array<NonNullable<W>>
-		: keyof T extends never
-		? Array<unknown>
-		: Array<NonNullable<T[keyof T]>>;
+	values<T>(this: void, o: ReadonlyArray<T>): Array<NonNullable<T>>;
+	values<T>(this: void, o: ReadonlySet<T>): Array<true>;
+	values<K, V>(this: void, o: ReadonlyMap<K, V>): Array<NonNullable<V>>;
+	values<T extends object>(this: void, o: T): keyof T extends never ? Array<unknown> : Array<NonNullable<T[keyof T]>>;
 
 	/**
 	 * Returns an array of key/values of the enumerable properties of an object
 	 * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
 	 */
+	entries<T>(this: void, o: ReadonlyArray<T>): Array<[number, NonNullable<T>]>;
+	entries<T>(this: void, o: ReadonlySet<T>): Array<[T, true]>;
+	entries<K, V>(this: void, o: ReadonlyMap<K, V>): Array<[K, NonNullable<V>]>;
 	entries<T extends object>(
 		this: void,
 		o: T,
-	): T extends Array<infer U>
-		? Array<[number, NonNullable<U>]>
-		: T extends ReadonlySet<infer E>
-		? Array<[E, true]>
-		: T extends ReadonlyMap<infer K, infer V>
-		? Array<[K, NonNullable<V>]>
-		: T extends ArrayLike<infer W>
-		? Array<[number, NonNullable<W>]>
-		: keyof T extends never
-		? Array<[unknown, unknown]>
-		: Array<[keyof T, NonNullable<T[keyof T]>]>;
+	): keyof T extends never ? Array<[unknown, unknown]> : Array<[keyof T, NonNullable<T[keyof T]>]>;
 
 	/** Creates an object from a set of entries */
 	fromEntries<P extends readonly [string | number | symbol, unknown]>(
